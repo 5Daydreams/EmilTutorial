e6a17a96b8ba9b2020ac2081af1bff90766f77d4
5daydreams <nelsonkossuga@gmail.com>
2022-03-27 00:39:47 +0100

    stopped at part 4, 1h 13mins;

---
+++ Source/CppEmilLectures/MyActor.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = Root;
+
+	Sphere = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollision"));
+	Sphere->SetupAttachment(Root);
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(Sphere);
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Log, TEXT("Hell ye, prints and shit"));
+}
+
+void AMyActor::SuperCrazyFunction()
+{
+	AddActorLocalOffset(FVector::UpVector * 500.0f);
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	FVector vec;
+	FRotator rot;
+
+	Super::Tick(DeltaTime);
+	AddActorWorldOffset(GetActorForwardVector() * Speed * DeltaTime);
+}
+
---
+++ Source/CppEmilLectures/MyActor.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class AMyActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AMyActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+		void SuperCrazyFunction();
+
+	UPROPERTY(VisibleAnywhere)
+		USceneComponent* Root;
+
+	UPROPERTY(VisibleAnywhere)
+		USphereComponent* Sphere;
+
+	UPROPERTY(VisibleAnywhere)
+		UStaticMeshComponent* Mesh;
+
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly)
+		float Speed = 200.0f;
+
+};
