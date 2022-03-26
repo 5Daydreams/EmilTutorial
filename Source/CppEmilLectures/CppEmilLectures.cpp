6a4a2b0745633a5e61c278305ede079bd8451755
5daydreams <nelsonkossuga@gmail.com>
2022-03-26 18:21:16 +0100

    partially remade the project, because the template was BP as opposed to cpp

---
+++ Source/CppEmilLectures/CppEmilLectures.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CppEmilLectures.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CppEmilLectures, "CppEmilLectures" );
+ 
---
+++ Source/CppEmilLectures/CppEmilLecturesGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CppEmilLecturesGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACppEmilLecturesGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACppEmilLecturesGameMode();
+};
+
+
+
